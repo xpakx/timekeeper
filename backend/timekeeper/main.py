c9ad663a7cd072a67281d3f9d021a16619c30659
xpakx <55586470+xpakx@users.noreply.github.com>
2023-06-01 14:25:30 +0200

    fix(backend): send 400 error instead of 422 when validation fails

--- backend/timekeeper/main.py
+++ backend/timekeeper/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from .routers import timer_router, user_router
 from .db.models import Base
 from .db.base import engine
@@ -9,3 +11,14 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(timer_router.router)
 app.include_router(user_router.router)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError
+):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"detail": exc.errors(), "body": exc.body},
+    )
