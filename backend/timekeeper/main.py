ce333d2fbd9cd4484488c38fbe1a153132ea0a41
xpakx <55586470+xpakx@users.noreply.github.com>
2023-06-07 12:32:21 +0200

    feat(frontend): let user register

--- backend/timekeeper/main.py
+++ backend/timekeeper/main.py
@@ -15,7 +15,7 @@ app.include_router(user_router.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
---
+++ frontend/src/routes/register/+page.svelte
@@ -0,0 +1,76 @@
+<svelte:head>
+    <title>Register</title>
+</svelte:head>
+
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    let apiUri = "http://localhost:8000";
+
+    let message: String;
+    let user = {username: "", password: "", repeated_password: ""};
+
+    async function register() {
+        const form = <HTMLFormElement> document.getElementById('register');
+
+        if (form && form.checkValidity()) {
+            try {
+                let response = await fetch(`${apiUri}/users/register`, {
+                    method: 'POST',
+                    body: JSON.stringify(user), 
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+
+                if (response.ok) {
+                    const fromEndpoint = await response.json()
+                    console.log("Success");
+                    goto('/')
+                } else {
+                    message = response.statusText;
+                }
+
+            } catch (err) {
+                if (err instanceof Error) {
+                    message = err.message;
+                }
+            }
+
+        }
+        
+    }
+</script>
+
+<h1>Register</h1>
+<form id="register" autocomplete="on" novalidate>
+    <div>
+        <label for="username">Username</label>
+        <input 
+        bind:value={user.username} 
+        required placeholder="Username" 
+        id="username"/>
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input type="password" 
+        id="password" 
+        bind:value={user.password} 
+        required 
+        placeholder="Password" />
+    </div>
+    <div>
+        <label for="password">Confirm password</label>
+        <input 
+        type="password" 
+        id="password" 
+        bind:value={user.repeated_password} 
+        required 
+        placeholder="Password (again)" />
+    </div>
+    
+    {#if message}
+        <p>{message}</p>
+    {/if}
+    
+    <button type="button" on:click={register}>Register</button>
+</form>
