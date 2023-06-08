808106913e8259e2b920ac6fda01fc0698ed3233
xpakx <55586470+xpakx@users.noreply.github.com>
2023-06-08 18:43:07 +0200

    feat(frontend): let user add new timers

---
+++ frontend/src/routes/add/+page.svelte
@@ -0,0 +1,74 @@
+<svelte:head>
+    <title>New timer</title>
+</svelte:head>
+
+<script lang="ts">
+    import { get } from 'svelte/store';
+    import { tokenStorage } from '../../storage'; 
+    import { goto } from '$app/navigation';
+    let apiUri = "http://localhost:8000";
+    let message: String;
+    let timer = {name: "", description: "", duration_s: 0};
+
+    async function addTimer() {
+        let token: String = get(tokenStorage);
+        const form = <HTMLFormElement> document.getElementById('new_timer');
+
+        if(token && form && form.checkValidity()) {
+            try {
+                let response = await fetch(`${apiUri}/timers/`, {
+                    method: 'POST',
+                    body: JSON.stringify(timer), 
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+
+                if (response.ok) {
+                    goto("/");
+                } else {
+                    const errorBody = await response.json();
+                    message = errorBody.detail;
+                }
+
+            } catch (err) {
+                if (err instanceof Error) {
+                    message = err.message;
+                }
+            }
+        }
+    }
+
+</script>
+
+<h1>New timer</h1>
+<form id="new_timer" autocomplete="on" novalidate>
+    <div>
+        <label for="name">Name</label>
+        <input 
+        bind:value={timer.name} 
+        required placeholder="Name" 
+        id="name"/>
+    </div>
+    <div>
+        <label for="description">Description</label>
+        <input 
+        bind:value={timer.description} 
+        required placeholder="Description" 
+        id="description"/>
+    </div>
+    <div>
+        <label for="duration_s">Duration</label>
+        <input 
+        bind:value={timer.duration_s} 
+        required placeholder="Duration" 
+        id="duration_s"/>
+    </div>
+    
+    {#if message}
+        <p>{message}</p>
+    {/if}
+    
+    <button type="button" on:click={addTimer}>Add</button>
+</form>
