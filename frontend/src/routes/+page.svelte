f9b7aea406d71fdd8de954203e9f8ba6f5d72d95
xpakx <55586470+xpakx@users.noreply.github.com>
2023-06-21 16:27:20 +0200

    feat(frontend/ui): style timers

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,14 +2,24 @@
     import { get } from "svelte/store";
     import { usernameStorage } from "../storage";
     import { goto } from "$app/navigation";
-    import { tweened } from "svelte/motion"
+    import { tweened } from "svelte/motion";
     import { getToken } from "../token-manager";
-    import Fa from 'svelte-fa'
-    import { faTrash, faHourglassStart, faEdit, faPlayCircle, faPlay, faStop, faCancel, faCheck } from '@fortawesome/free-solid-svg-icons'
+    import Fa from "svelte-fa";
+    import {
+        faTrash,
+        faHourglassStart,
+        faEdit,
+        faPlayCircle,
+        faPlay,
+        faStop,
+        faCancel,
+        faCheck,
+        faAdd,
+    } from "@fortawesome/free-solid-svg-icons";
     let username: String = get(usernameStorage);
     let apiUri = "http://localhost:8000";
     let message: String;
-    let date = tweened(Date.now(), {duration: 500});
+    let date = tweened(Date.now(), { duration: 500 });
     setInterval(() => {
         $date = Date.now();
         testTimers();
@@ -29,11 +39,11 @@
         end_time?: Date;
         state: String;
         timer_id: number;
-	timer: {
-		name: string;
-		duration_s: number;
-        autofinish: boolean;
-	};
+        timer: {
+            name: string;
+            duration_s: number;
+            autofinish: boolean;
+        };
     }[];
 
     usernameStorage.subscribe((value) => {
@@ -44,7 +54,7 @@
 
     async function getAllTimers() {
         let token: String = await getToken();
-        if(!token || token == '') {
+        if (!token || token == "") {
             return;
         }
 
@@ -61,7 +71,7 @@
                 timers = await response.json();
             } else {
                 if (response.status == 401) {
-                    goto('/logout');
+                    goto("/logout");
                 }
                 const errorBody = await response.json();
                 message = errorBody.detail;
@@ -75,7 +85,7 @@
 
     async function getActiveTimers() {
         let token: String = await getToken();
-        if(!token || token == '') {
+        if (!token || token == "") {
             return;
         }
 
@@ -94,17 +104,15 @@
                     return {
                         id: t.id,
                         start_time: new Date(t.start_time),
-                        end_time: t.end_time
-                            ? new Date(t.end_time)
-                            : undefined,
+                        end_time: t.end_time ? new Date(t.end_time) : undefined,
                         state: t.state,
                         timer_id: t.timer_id,
-                        timer: t.timer
+                        timer: t.timer,
                     };
                 });
             } else {
                 if (response.status == 401) {
-                    goto('/logout');
+                    goto("/logout");
                 }
                 const errorBody = await response.json();
                 message = errorBody.detail;
@@ -126,7 +134,7 @@
 
     async function deleteTimer(id: number) {
         let token: String = await getToken();
-        if(!token || token == '') {
+        if (!token || token == "") {
             return;
         }
 
@@ -143,7 +151,7 @@
                 timers = timers.filter((a) => a.id != id);
             } else {
                 if (response.status == 401) {
-                    goto('/logout');
+                    goto("/logout");
                 }
                 const errorBody = await response.json();
                 message = errorBody.detail;
@@ -157,7 +165,7 @@
 
     async function startTimer(id: number) {
         let token: String = await getToken();
-        if(!token || token == '') {
+        if (!token || token == "") {
             return;
         }
 
@@ -176,7 +184,7 @@
                 running_timers = [...running_timers, new_timer];
             } else {
                 if (response.status == 401) {
-                    goto('/logout');
+                    goto("/logout");
                 }
                 const errorBody = await response.json();
                 message = errorBody.detail;
@@ -190,28 +198,31 @@
 
     async function changeTimerState(id: number, state: String) {
         let token: String = await getToken();
-        if(!token || token == '') {
+        if (!token || token == "") {
             return;
         }
 
         let body = {
-            "state": state
+            state: state,
         };
         try {
-            let response = await fetch(`${apiUri}/timers/instances/${id}/state`, {
-                method: "POST",
-                body: JSON.stringify(body),
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`,
-                },
-            });
+            let response = await fetch(
+                `${apiUri}/timers/instances/${id}/state`,
+                {
+                    method: "POST",
+                    body: JSON.stringify(body),
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
 
             if (response.ok) {
                 running_timers = running_timers.filter((a) => a.id != id);
             } else {
                 if (response.status == 401) {
-                    goto('/logout');
+                    goto("/logout");
                 }
                 const errorBody = await response.json();
                 message = errorBody.detail;
@@ -223,7 +234,6 @@
         }
     }
 
-
     function testTimers() {
         if (!running_timers) {
             return;
@@ -231,10 +241,10 @@
 
         running_timers.forEach((t) => {
             if (
-                ($date - t.start_time.getTime()) > (1000*t.timer.duration_s) 
-                && 
-                ($date - t.start_time.getTime() - 500) <= (1000*t.timer.duration_s) )
-            {
+                $date - t.start_time.getTime() > 1000 * t.timer.duration_s &&
+                $date - t.start_time.getTime() - 500 <=
+                    1000 * t.timer.duration_s
+            ) {
                 if (audio.paused) {
                     audio.play();
                 }
@@ -243,7 +253,6 @@
                 }
             }
         });
-
     }
 </script>
 
@@ -262,23 +271,45 @@
     <h2>Running</h2>
 
     {#each running_timers as timer}
-        <div class="timer-container {
-        ($date - timer.start_time.getTime()) > (1000*timer.timer.duration_s) ? 'finished' : ''
-        }">
-           <span class="timer-name"> {timer.timer.name}</span>
-            <progress value={(($date - timer.start_time.getTime())/(1000*timer.timer.duration_s))}></progress>
-            {#if ($date - timer.start_time.getTime()) <= (1000*timer.timer.duration_s)}
-                <button class="btn-icon" type="button" on:click={() => changeTimerState(timer.id, "cancelled")}>
-                    <Fa icon={faStop} />
+        <div
+            class="timer-container {$date - timer.start_time.getTime() >
+            1000 * timer.timer.duration_s
+                ? 'finished'
+                : ''}"
+        >
+            <span class="timer-name"> {timer.timer.name}</span>
+            <div class="progress-bar">
+                <progress
+                    value={($date - timer.start_time.getTime()) /
+                        (1000 * timer.timer.duration_s)}
+                />
+            </div>
+            <div class="buttons">
+                {#if $date - timer.start_time.getTime() <= 1000 * timer.timer.duration_s}
+                    <button
+                        class="btn-icon"
+                        type="button"
+                        on:click={() => changeTimerState(timer.id, "cancelled")}
+                    >
+                        <Fa icon={faStop} />
+                    </button>
+                {:else}
+                    <button
+                        type="button"
+                        class="btn-finished btn-icon"
+                        on:click={() => changeTimerState(timer.id, "finished")}
+                    >
+                        <Fa icon={faCheck} />
+                    </button>
+                {/if}
+                <button
+                    type="button"
+                    class="btn-fail btn-icon"
+                    on:click={() => changeTimerState(timer.id, "failed")}
+                >
+                    <Fa icon={faCancel} />
                 </button>
-            {:else}
-                <button type="button" class="btn-finished btn-icon" on:click={() => changeTimerState(timer.id, "finished")}>
-                    <Fa icon={faCheck} />
-                </button>
-            {/if}
-            <button type="button" class="btn-fail btn-icon" on:click={() => changeTimerState(timer.id, "failed")}>
-                <Fa icon={faCancel} />
-            </button>
+            </div>
         </div>
     {/each}
 {/if}
@@ -287,73 +318,101 @@
 {#if timers}
     {#each timers as timer}
         <div class="timer-container">
-            {timer.name}
-            <button class="btn-icon" type="button" on:click={() => deleteTimer(timer.id)}>
-                <Fa icon={faTrash} />
-            </button >
-            <button class="btn-icon" type="button" on:click={() => goto(`/edit/${timer.id}`)}>
-                <Fa icon={faEdit} />
-            </button>
-            <button class="btn-icon" type="button" on:click={() => startTimer(timer.id)}>
-                <Fa icon={faPlay} />
-            </button>
+            <span class="timer-name"> {timer.name}</span>
+            <div class="buttons">
+                <button
+                    class="btn-icon"
+                    type="button"
+                    on:click={() => deleteTimer(timer.id)}
+                >
+                    <Fa icon={faTrash} />
+                </button>
+                <button
+                    class="btn-icon"
+                    type="button"
+                    on:click={() => goto(`/edit/${timer.id}`)}
+                >
+                    <Fa icon={faEdit} />
+                </button>
+                <button
+                    class="btn-icon"
+                    type="button"
+                    on:click={() => startTimer(timer.id)}
+                >
+                    <Fa icon={faPlay} />
+                </button>
+            </div>
         </div>
     {/each}
 
-    <button type="button" on:click={add}>Add</button>
+    <button type="button" class="btn-icon" on:click={add}>
+        <Fa icon={faAdd} />
+    </button>
 {/if}
 {#if !timers || timers.length == 0}
     <span>No timers</span>
 {/if}
 
-<audio src="https://freesound.org/data/previews/536/536420_4921277-lq.mp3" bind:this={audio}></audio>
+<audio
+    src="https://freesound.org/data/previews/536/536420_4921277-lq.mp3"
+    bind:this={audio}
+/>
+
 <style>
+    .finished {
+        color: #a6adc8;
+    }
 
-.finished {
-    color: #a6adc8;
-}
+    h2 {
+        font-size: 24px;
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
 
-h2 {
-    font-size: 24px;
-    font-weight: bold;
-    margin-bottom: 10px;
-}
-    
-    
-button {
-    font-size: 14px;
-    padding: 5px 10px;
-    margin-right: 10px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    background-color: #9399b2;
-    color: #313244;
-}
+    .timer-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        background-color: #181825;
+        border-radius: 8px;
+        padding: 10px;
+        margin-bottom: 10px;
+    }
 
-.finished {
-    opacity: 0.5;
-}
+    button {
+        font-size: 14px;
+        padding: 5px 10px;
+        margin-right: 10px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        background-color: #9399b2;
+        color: #313244;
+    }
 
-.btn-finished {
-    background-color: #a6e3a1;
-    color: white;
-}
+    .finished {
+        opacity: 0.5;
+    }
 
-.btn-finished:hover {
-    background-color: #94e2d5;
-}
+    .btn-finished {
+        background-color: #a6e3a1;
+        color: white;
+    }
 
-button.btn-fail {
-    background-color: #f38ba8;
-    color: white;
-}
+    .btn-finished:hover {
+        background-color: #94e2d5;
+    }
 
-button.btn-fail:hover {
-    background-color: #eba0ac;
-}
+    button.btn-fail {
+        background-color: #f38ba8;
+        color: white;
+    }
 
-button.btn-icon {
-    border-radius: 7px;
-}
+    button.btn-fail:hover {
+        background-color: #eba0ac;
+    }
+
+    button.btn-icon {
+        border-radius: 7px;
+    }
 </style>
