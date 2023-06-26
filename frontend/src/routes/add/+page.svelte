2b9122eeb6c2bade714909fcedd37dbf1d0cce5c
xpakx <55586470+xpakx@users.noreply.github.com>
2023-06-26 20:13:52 +0200

    feat(frontend/ui): style checkboxes

--- frontend/src/routes/add/+page.svelte
+++ frontend/src/routes/add/+page.svelte
@@ -1,152 +1,213 @@
-<svelte:head>
-    <title>New timer</title>
-</svelte:head>
-
 <script lang="ts">
-    import { goto } from '$app/navigation';
-    import { getToken } from '../../token-manager';
+    import { goto } from "$app/navigation";
+    import { getToken } from "../../token-manager";
 
     let apiUri = "http://localhost:8000";
     let message: String;
-    let timer = {name: "", description: "", duration_s: 0, autofinish: false};
+    let timer = { name: "", description: "", duration_s: 0, autofinish: false };
 
     async function addTimer() {
         let token: String = await getToken();
-        const form = <HTMLFormElement> document.getElementById('new_timer');
+        const form = <HTMLFormElement>document.getElementById("new_timer");
 
-        if(!token || token == '' || !form || !form.checkValidity()) {
+        if (!token || token == "" || !form || !form.checkValidity()) {
             return;
         }
 
         try {
             let response = await fetch(`${apiUri}/timers/`, {
-                method: 'POST',
-                body: JSON.stringify(timer), 
+                method: "POST",
+                body: JSON.stringify(timer),
                 headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
             });
 
             if (response.ok) {
                 goto("/");
             } else {
                 if (response.status == 401) {
-                    goto('/logout');
+                    goto("/logout");
                 }
                 const errorBody = await response.json();
                 message = errorBody.detail;
             }
-
         } catch (err) {
             if (err instanceof Error) {
                 message = err.message;
             }
         }
     }
-
 </script>
+
+<svelte:head>
+    <title>New timer</title>
+</svelte:head>
 
 <h1>New timer</h1>
 <form id="new_timer" autocomplete="on" novalidate>
     <div>
         <label for="name">Name</label>
-        <input 
-        bind:value={timer.name} 
-        required placeholder="Name" 
-        id="name"/>
+        <input bind:value={timer.name} required placeholder="Name" id="name" />
     </div>
     <div>
         <label for="description">Description</label>
-        <input 
-        bind:value={timer.description} 
-        required placeholder="Description" 
-        id="description"/>
+        <input
+            bind:value={timer.description}
+            required
+            placeholder="Description"
+            id="description"
+        />
     </div>
     <div>
         <label for="duration_s">Duration</label>
-        <input 
-        bind:value={timer.duration_s} 
-        required placeholder="Duration" 
-        id="duration_s"/>
+        <input
+            bind:value={timer.duration_s}
+            required
+            placeholder="Duration"
+            id="duration_s"
+        />
     </div>
-    <div>
-        <label for="autofinish">Autofinish</label>
-        <input type=checkbox bind:checked={timer.autofinish} id="autofinish">
+    <div class="checkbox-container">
+        <input
+            type="checkbox"
+            bind:checked={timer.autofinish}
+            id="autofinish"
+        />
+        <label for="autofinish" class="checkbox-label">
+            <div class="checkbox" />
+            Autofinish
+        </label>
     </div>
-    
+
     {#if message}
         <p>{message}</p>
     {/if}
-    
+
     <button type="button" on:click={addTimer}>Add</button>
 </form>
 
 <style>
-form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-  background-color: #181825;
-  border-radius: 5px;
-}
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 20px;
+        background-color: #181825;
+        border-radius: 5px;
+    }
 
-form div {
-  margin-bottom: 15px;
-  width: 100%;
-}
+    form div {
+        margin-bottom: 15px;
+        width: 100%;
+    }
 
-label {
-  display: block;
-  font-weight: bold;
-  margin-bottom: 5px;
-}
+    label {
+        display: block;
+        font-weight: bold;
+        margin-bottom: 5px;
+    }
 
-input {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #11111b;
-  border-radius: 4px;
-  box-sizing: border-box;
-  font-family: inherit;
-  font-size: 14px;
-  transition: border-color 0.3s ease;
-  background-color: #1e1e2e;
-  color: #a6adc8;
-}
+    input {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #11111b;
+        border-radius: 4px;
+        box-sizing: border-box;
+        font-family: inherit;
+        font-size: 14px;
+        transition: border-color 0.3s ease;
+        background-color: #1e1e2e;
+        color: #a6adc8;
+    }
 
-input:focus {
-  outline: none;
-  border-color: #f2cdcd;
-  box-shadow: 0 0 3px #f2cdcd;
-}
+    input:focus {
+        outline: none;
+        border-color: #f2cdcd;
+        box-shadow: 0 0 3px #f2cdcd;
+    }
 
-input:hover {
-  border-color: #f2cdcd
-}
+    input:hover {
+        border-color: #f2cdcd;
+    }
 
-input:disabled {
-  background-color: #313244;
-  cursor: not-allowed;
-}
+    input:disabled {
+        background-color: #313244;
+        cursor: not-allowed;
+    }
 
+    p {
+        color: #f38ba8;
+        margin-top: 10px;
+    }
 
-p {
-  color: #f38ba8;
-  margin-top: 10px;
-}
+    button {
+        padding: 10px 20px;
+        background-color: #9399b2;
+        color: #313244;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
 
-button {
-  padding: 10px 20px;
-  background-color: #9399b2;
-  color: #313244;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
+    button:hover {
+        background-color: #7f849c;
+    }
 
-button:hover {
-  background-color: #7f849c;
-}
+    .checkbox-container label {
+        display: flex;
+        align-items: start;
+    }
+
+    .checkbox-label {
+        color: #a6adc8;
+        font-size: 14px;
+    }
+
+    .checkbox {
+        position: relative;
+        width: 16px;
+        height: 16px;
+        border: 1px solid #11111b;
+        border-radius: 3px;
+        transition: border-color 0.3s ease;
+        background-color: #1e1e2e;
+        margin-right: 10px;
+        cursor: pointer;
+    }
+
+    .checkbox:before {
+        content: "";
+        position: absolute;
+        top: 3px;
+        left: 3px;
+        width: 10px;
+        height: 10px;
+        border-radius: 2px;
+        background-color: #f2cdcd;
+        opacity: 0;
+        transition: all 0.3s ease;
+    }
+
+    input[type="checkbox"]:checked + label .checkbox:before {
+        opacity: 1;
+    }
+
+    input[type="checkbox"] {
+        display: none;
+    }
+
+    .checkbox:hover {
+        border-color: #f2cdcd;
+    }
+
+    .checkbox:hover:before {
+        border-radius: 5px;
+    }
+
+    .checkbox:focus {
+        outline: none;
+        box-shadow: 0 0 3px #f2cdcd;
+    }
 </style>
--- frontend/src/routes/edit/[id]/+page.svelte
+++ frontend/src/routes/edit/[id]/+page.svelte
@@ -1,44 +1,45 @@
-<svelte:head>
-    <title>Edit timer</title>
-</svelte:head>
-
 <script lang="ts">
-    import { goto } from '$app/navigation';
-    import { page } from '$app/stores';
-    import { getToken } from '../../../token-manager';
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
+    import { getToken } from "../../../token-manager";
     let id = Number($page.params.id);
     let apiUri = "http://localhost:8000";
     let message: String;
-    let timer = {id: 0, name: "", description: "", duration_s: 0, autofinish: false};
+    let timer = {
+        id: 0,
+        name: "",
+        description: "",
+        duration_s: 0,
+        autofinish: false,
+    };
     getTimer(id);
 
     async function editTimer() {
         let token: String = await getToken();
-        const form = <HTMLFormElement> document.getElementById('edit_timer');
-        if(!token || token == '' || !form || !form.checkValidity()) {
+        const form = <HTMLFormElement>document.getElementById("edit_timer");
+        if (!token || token == "" || !form || !form.checkValidity()) {
             return;
         }
 
         try {
             let response = await fetch(`${apiUri}/timers/${id}`, {
-                method: 'PUT',
-                body: JSON.stringify(timer), 
+                method: "PUT",
+                body: JSON.stringify(timer),
                 headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
             });
 
             if (response.ok) {
                 goto("/");
             } else {
                 if (response.status == 401) {
-                    goto('/logout');
+                    goto("/logout");
                 }
                 const errorBody = await response.json();
                 message = errorBody.detail;
             }
-
         } catch (err) {
             if (err instanceof Error) {
                 message = err.message;
@@ -49,29 +50,28 @@
     async function getTimer(id: number) {
         let token: String = await getToken();
 
-        if(!token || token == '') {
+        if (!token || token == "") {
             return;
         }
 
         try {
             let response = await fetch(`${apiUri}/timers/${id}`, {
-                method: 'GET',
+                method: "GET",
                 headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
             });
 
             if (response.ok) {
                 timer = await response.json();
             } else {
                 if (response.status == 401) {
-                    goto('/logout');
+                    goto("/logout");
                 }
                 const errorBody = await response.json();
                 message = errorBody.detail;
             }
-
         } catch (err) {
             if (err instanceof Error) {
                 message = err.message;
@@ -80,106 +80,174 @@
     }
 </script>
 
+<svelte:head>
+    <title>Edit timer</title>
+</svelte:head>
+
 <h1>Edit timer</h1>
 <form id="edit_timer" autocomplete="on" novalidate>
     <div>
         <label for="name">Name</label>
-        <input 
-        bind:value={timer.name} 
-        required placeholder="Name" 
-        id="name"/>
+        <input bind:value={timer.name} required placeholder="Name" id="name" />
     </div>
     <div>
         <label for="description">Description</label>
-        <input 
-        bind:value={timer.description} 
-        required placeholder="Description" 
-        id="description"/>
+        <input
+            bind:value={timer.description}
+            required
+            placeholder="Description"
+            id="description"
+        />
     </div>
     <div>
         <label for="duration_s">Duration</label>
-        <input 
-        bind:value={timer.duration_s} 
-        required placeholder="Duration" 
-        id="duration_s"/>
+        <input
+            bind:value={timer.duration_s}
+            required
+            placeholder="Duration"
+            id="duration_s"
+        />
     </div>
-    <div>
-        <label for="autofinish">Autofinish</label>
-        <input type=checkbox bind:checked={timer.autofinish} id="autofinish">
+    <div class="checkbox-container">
+        <input
+            type="checkbox"
+            bind:checked={timer.autofinish}
+            id="autofinish"
+        />
+        <label for="autofinish" class="checkbox-label">
+            <div class="checkbox" />
+            Autofinish
+        </label>
     </div>
-    
+
     {#if message}
         <p>{message}</p>
     {/if}
-    
+
     <button type="button" on:click={editTimer}>Save</button>
 </form>
 
 <style>
-form {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-  background-color: #181825;
-  border-radius: 5px;
-}
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 20px;
+        background-color: #181825;
+        border-radius: 5px;
+    }
 
-form div {
-  margin-bottom: 15px;
-  width: 100%;
-}
+    form div {
+        margin-bottom: 15px;
+        width: 100%;
+    }
 
-label {
-  display: block;
-  font-weight: bold;
-  margin-bottom: 5px;
-}
+    label {
+        display: block;
+        font-weight: bold;
+        margin-bottom: 5px;
+    }
 
-input {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #11111b;
-  border-radius: 4px;
-  box-sizing: border-box;
-  font-family: inherit;
-  font-size: 14px;
-  transition: border-color 0.3s ease;
-  background-color: #1e1e2e;
-  color: #a6adc8;
-}
+    input {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #11111b;
+        border-radius: 4px;
+        box-sizing: border-box;
+        font-family: inherit;
+        font-size: 14px;
+        transition: border-color 0.3s ease;
+        background-color: #1e1e2e;
+        color: #a6adc8;
+    }
 
-input:focus {
-  outline: none;
-  border-color: #f2cdcd;
-  box-shadow: 0 0 3px #f2cdcd;
-}
+    input:focus {
+        outline: none;
+        border-color: #f2cdcd;
+        box-shadow: 0 0 3px #f2cdcd;
+    }
 
-input:hover {
-  border-color: #f2cdcd
-}
+    input:hover {
+        border-color: #f2cdcd;
+    }
 
-input:disabled {
-  background-color: #313244;
-  cursor: not-allowed;
-}
+    input:disabled {
+        background-color: #313244;
+        cursor: not-allowed;
+    }
 
+    p {
+        color: #f38ba8;
+        margin-top: 10px;
+    }
 
-p {
-  color: #f38ba8;
-  margin-top: 10px;
-}
+    button {
+        padding: 10px 20px;
+        background-color: #9399b2;
+        color: #313244;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
 
-button {
-  padding: 10px 20px;
-  background-color: #9399b2;
-  color: #313244;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
+    button:hover {
+        background-color: #7f849c;
+    }
 
-button:hover {
-  background-color: #7f849c;
-}
+    /* Custom checkbox style */
+    .checkbox-container label {
+        display: flex;
+        align-items: start;
+    }
+
+    .checkbox-label {
+        color: #a6adc8;
+        font-size: 14px;
+    }
+
+    .checkbox {
+        position: relative;
+        width: 16px;
+        height: 16px;
+        border: 1px solid #11111b;
+        border-radius: 3px;
+        transition: border-color 0.3s ease;
+        background-color: #1e1e2e;
+        margin-right: 10px;
+        cursor: pointer;
+    }
+
+    .checkbox:before {
+        content: "";
+        position: absolute;
+        top: 3px;
+        left: 3px;
+        width: 10px;
+        height: 10px;
+        border-radius: 2px;
+        background-color: #f2cdcd;
+        opacity: 0;
+        transition: all 0.3s ease;
+    }
+
+    input[type="checkbox"]:checked + label .checkbox:before {
+        opacity: 1;
+    }
+
+    input[type="checkbox"] {
+        display: none;
+    }
+
+    .checkbox:hover {
+        border-color: #f2cdcd;
+    }
+
+    .checkbox:hover:before {
+        border-radius: 5px;
+    }
+
+    .checkbox:focus {
+        outline: none;
+        box-shadow: 0 0 3px #f2cdcd;
+    }
 </style>
