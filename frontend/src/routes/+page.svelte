b7a7fbfecfac4362ff84933ce282f31b14915b34
xpakx <55586470+xpakx@users.noreply.github.com>
2023-06-30 19:36:18 +0200

    refactor(frontend): extract running timer to component

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,19 +5,13 @@
     import { getToken } from "../token-manager";
     import Fa from "svelte-fa";
     import {
-        faTrash,
-        faEdit,
-        faPlay,
-        faStop,
-        faCancel,
-        faCheck,
         faAdd,
         faArrowLeft,
         faArrowRight,
     } from "@fortawesome/free-solid-svg-icons";
-    import { fade } from "svelte/transition";
     import { onDestroy } from "svelte";
     import Timer from "../components/Timer.svelte";
+    import RunningTimer from "../components/RunningTimer.svelte";
 
     let apiUri = "http://localhost:8000";
     let message: String;
@@ -278,47 +272,11 @@
     <h2>Running</h2>
 
     {#each running_timers as timer}
-        <div
-            class="timer-container {$date - timer.start_time.getTime() >
-            1000 * timer.timer.duration_s
-                ? 'finished'
-                : ''}"
-            transition:fade|local
-        >
-            <span class="timer-name"> {timer.timer.name}</span>
-            <div class="progress-bar">
-                <progress
-                    value={($date - timer.start_time.getTime()) /
-                        (1000 * timer.timer.duration_s)}
-                />
-            </div>
-            <div class="buttons">
-                {#if $date - timer.start_time.getTime() <= 1000 * timer.timer.duration_s}
-                    <button
-                        class="btn-icon"
-                        type="button"
-                        on:click={() => changeTimerState(timer.id, "cancelled")}
-                    >
-                        <Fa icon={faStop} />
-                    </button>
-                {:else}
-                    <button
-                        type="button"
-                        class="btn-finished btn-icon"
-                        on:click={() => changeTimerState(timer.id, "finished")}
-                    >
-                        <Fa icon={faCheck} />
-                    </button>
-                {/if}
-                <button
-                    type="button"
-                    class="btn-fail btn-icon"
-                    on:click={() => changeTimerState(timer.id, "failed")}
-                >
-                    <Fa icon={faCancel} />
-                </button>
-            </div>
-        </div>
+        <RunningTimer
+            bind:date={$date}
+            {timer}
+            on:state={(event) => changeTimerState(timer.id, event.detail.state)}
+        />
     {/each}
 {/if}
 
@@ -362,32 +320,10 @@
 />
 
 <style>
-    .finished {
-        color: #a6adc8;
-    }
-
     h2 {
         font-size: 24px;
         font-weight: bold;
         margin-bottom: 10px;
-    }
-
-    .timer-container {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        background-color: #181825;
-        border-radius: 8px;
-        padding: 10px;
-        margin-bottom: 10px;
-    }
-
-    .timer-container .timer-name {
-        margin-bottom: 5px;
-    }
-
-    .timer-container .progress-bar {
-        margin-bottom: 5px;
     }
 
     button {
@@ -399,81 +335,6 @@
         cursor: pointer;
         background-color: #9399b2;
         color: #313244;
-    }
-
-    .finished {
-        opacity: 0.5;
-    }
-
-    .btn-finished {
-        background-color: #a6e3a1;
-        color: white;
-    }
-
-    .btn-finished:hover {
-        background-color: #94e2d5;
-    }
-
-    button.btn-fail {
-        background-color: #f38ba8;
-        color: white;
-    }
-
-    button.btn-fail:hover {
-        background-color: #eba0ac;
-    }
-
-    progress {
-        height: 20px;
-        border: none;
-        border-radius: 10px;
-        background-color: #1e1e2e;
-        overflow: hidden;
-        box-shadow: inset 1px 1px 3px 0 rgba(0, 0, 0, 0.8),
-            1px 1px 0 0 rgba(255, 255, 255, 0.12);
-    }
-
-    progress::-webkit-progress-bar {
-        background-color: #1e1e2e;
-        border-radius: 10px;
-    }
-
-    progress::-webkit-progress-value {
-        background-image: linear-gradient(
-                to bottom,
-                rgba(255, 255, 255, 0.33) 0,
-                rgba(255, 255, 255, 0.08) 50%,
-                rgba(0, 0, 0, 0.25) 50%,
-                rgba(0, 0, 0, 0.1) 100%
-            ),
-            linear-gradient(to right, #e67070, #f2cdcd);
-    }
-
-    progress::-moz-progress-bar {
-        background-image: linear-gradient(
-                to bottom,
-                rgba(255, 255, 255, 0.33) 0,
-                rgba(255, 255, 255, 0.08) 50%,
-                rgba(0, 0, 0, 0.25) 50%,
-                rgba(0, 0, 0, 0.1) 100%
-            ),
-            linear-gradient(to right, #e67070, #f2cdcd);
-    }
-
-    progress::-ms-fill {
-        background-image: linear-gradient(
-                to bottom,
-                rgba(255, 255, 255, 0.33) 0,
-                rgba(255, 255, 255, 0.08) 50%,
-                rgba(0, 0, 0, 0.25) 50%,
-                rgba(0, 0, 0, 0.1) 100%
-            ),
-            linear-gradient(to right, #e67070, #f2cdcd);
-    }
-
-    progress::-ms-fill-upper {
-        background-color: #1e1e2e;
-        border-radius: 10px;
     }
 
     button.btn-icon {
