ae5b69caa5f21699e7af42b35e30928f9d60c21b
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-11 19:35:47 +0200

    feat(backend): return correct errors while generating reward

--- backend/timekeeper/services/reward_service.py
+++ backend/timekeeper/services/reward_service.py
@@ -1,13 +1,28 @@
 from ..db import item_repo, timer_repo, equipment_repo
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 
 def get_reward(timer_id: int, user_id: int, db: Session):
     timer = timer_repo.get_timer_inst(timer_id, user_id, db)
     if not timer.reward_time:
-        return
+        raise not_reward_time_exception()
     item = item_repo.get_random_item(db)
     if not item:
-        return
+        raise not_initialized_exception()
     equipment_repo.create_entry(item.id, 1, user_id, db)
     return item
+
+
+def not_initialized_exception():
+    return HTTPException(
+        status_code=500,
+        detail="Rewards not initialized",
+    )
+
+
+def not_reward_time_exception():
+    return HTTPException(
+        status_code=400,
+        detail="Cannot get reward from this timer",
+    )
