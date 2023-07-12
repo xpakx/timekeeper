365e1353a34f975454a3bb724bc495bcf75f2124
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-12 22:26:31 +0200

    feat(backend): prevent claiming two rewards for the same timer instance

--- backend/timekeeper/services/reward_service.py
+++ backend/timekeeper/services/reward_service.py
@@ -7,10 +7,14 @@ def get_reward(timer_id: int, user_id: int, db: Session):
     timer = timer_repo.get_timer_inst(timer_id, user_id, db)
     if not timer.reward_time:
         raise not_reward_time_exception()
+    if timer.rewarded:
+        raise already_rewarded_exception()
     item = item_repo.get_random_item(db)
     if not item:
         raise not_initialized_exception()
     equipment_repo.create_entry(item.id, 1, user_id, db)
+    timer.rewarded = True
+    db.commit()
     return item
 
 
@@ -25,4 +29,11 @@ def not_reward_time_exception():
     return HTTPException(
         status_code=400,
         detail="Cannot get reward from this timer",
+    )
+
+
+def already_rewarded_exception():
+    return HTTPException(
+        status_code=403,
+        detail="Cannot reward twice",
     )
