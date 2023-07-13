7c1751ca8c3cf0f1ef7ee0e03572bf2e9b21544b
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-13 11:17:02 +0200

    feat(backend): prevent duplicating equipment entries

--- backend/timekeeper/db/equipment_repo.py
+++ backend/timekeeper/db/equipment_repo.py
@@ -1,12 +1,22 @@
 from .models import EquipmentEntry
 from sqlalchemy.orm import Session
+from sqlalchemy import and_
 
 
 def create_entry(item_id, amount, user_id, db: Session):
-    entry = EquipmentEntry(
-            item_id=item_id,
-            amount=amount,
-            owner_id=user_id
-            )
-    db.add(entry)
-    db.commit()
+    entry_db = db\
+        .query(EquipmentEntry)\
+        .where(
+             and_(
+                 EquipmentEntry.owner_id == user_id,
+                 EquipmentEntry.item_id == item_id)
+            ) .first()
+    if entry_db:
+        entry_db.amount = entry_db.amount + 1
+    else:
+        entry = EquipmentEntry(
+                item_id=item_id,
+                amount=amount,
+                owner_id=user_id
+                )
+        db.add(entry)
