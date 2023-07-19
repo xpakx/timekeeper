75affbb74a828d4f1a34f61f2db7580068c2adab
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-19 21:30:23 +0200

    refactor(frontend): extract info message to separate component

--- frontend/src/components/InfoBar.svelte
+++ frontend/src/components/InfoBar.svelte
@@ -2,6 +2,7 @@
     import Fa from "svelte-fa";
     import type { InfoDetails } from "../types/InfoDetails";
     import { faClose } from "@fortawesome/free-solid-svg-icons";
+    import InfoMessage from "./InfoMessage.svelte";
 
     export const addInfo = (info: InfoDetails) => addNewInfo(info);
     let infos: InfoDetails[] = [];
@@ -26,22 +27,7 @@
     <button class="clean" on:click={closeAllMessages}>Clean</button>
     <div class="infos">
         {#each infos as info, index}
-            <div class="info {info.reward ? info.reward.rarity : ''}">
-                <div class="info-header">
-                    Reward
-                    <button
-                        on:click={() => closeMessage(index)}
-                        class="btn-icon"
-                    >
-                        <Fa icon={faClose} />
-                    </button>
-                </div>
-                {#if info.points}
-                    {info.points} points
-                {:else if info.reward}
-                    {info.reward.name}
-                {/if}
-            </div>
+            <InfoMessage {info} on:delete={() => closeMessage(index)} />
         {/each}
     </div>
 {/if}
@@ -54,23 +40,6 @@
         margin-top: 5px;
     }
 
-    .info {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        background-color: #181825;
-        border-radius: 8px;
-        padding: 10px;
-        margin-bottom: 10px;
-    }
-
-    .info-header {
-        font-weight: bold;
-        color: #a6adc8;
-        display: flex;
-        justify-content: space-between;
-    }
-
     button {
         font-size: 12;
         padding: 2px;
@@ -79,30 +48,5 @@
         cursor: pointer;
         background-color: #9399b2;
         color: #313244;
-    }
-
-    button.btn-icon {
-        background-color: transparent;
-        color: #9399b2;
-    }
-
-    .common {
-        border: solid 1px #585b70;
-    }
-
-    .uncommon {
-        border: solid 1px #cdd6f4;
-    }
-
-    .rare {
-        border: solid 1px #f2cdcd;
-    }
-
-    .info.uncommon .info-header {
-        color: #cdd6f4;
-    }
-
-    .info.rare .info-header {
-        color: #f2cdcd;
     }
 </style>
