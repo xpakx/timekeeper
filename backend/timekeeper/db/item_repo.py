2867cb640333a62655ba9e51d014ef6ccf7e9cce
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-20 18:27:21 +0200

    feat(backend): randomize rewards based on rarity

--- backend/timekeeper/db/item_repo.py
+++ backend/timekeeper/db/item_repo.py
@@ -2,9 +2,20 @@ from .models import Item
 from sqlalchemy.orm import Session
 import random
 
+COMMON = range(1, 6)
+UNCOMMON = range(6, 16)
+RARE = range(16, 21)
+
 
 def get_random_item(db: Session):
-    rnd = random.randint(1, 21)
+    rnd = random.randint(0, 6)
+    if rnd < 3:
+        rnd = random.choice(COMMON)
+    elif rnd < 5:
+        rnd = random.choice(UNCOMMON)
+    else:
+        rnd = random.choice(RARE)
+
     return db\
         .query(Item)\
         .where(
