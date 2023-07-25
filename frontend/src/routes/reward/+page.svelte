cfc212af1bf13bff949ff5085f9deb554c0011ff
xpakx <55586470+xpakx@users.noreply.github.com>
2023-07-25 14:26:41 +0200

    feat(frontend): let user generate heroes

--- frontend/src/routes/reward/+page.svelte
+++ frontend/src/routes/reward/+page.svelte
@@ -1,8 +1,51 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
+    import { getToken } from "../../token-manager";
+    import type { Hero } from "../../types/Hero";
+
+    let apiUri = "http://localhost:8000";
+    let hero: Hero[];
+    let message: String;
+
+    async function generateHero() {
+        let token: String = await getToken();
+        if (!token || token == "") {
+            return;
+        }
+
+        try {
+            let response = await fetch(
+                `${apiUri}/heroes/reward`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+
+            if (response.ok) {
+                let fromEndpoint = await response.json();
+                hero = fromEndpoint;
+            } else {
+                if (response.status == 401) {
+                    goto("/logout");
+                }
+                const errorBody = await response.json();
+                message = errorBody.detail;
+            }
+        } catch (err) {
+            if (err instanceof Error) {
+                message = err.message;
+            }
+        }
+    }
 </script>
 
 <svelte:head>
-    <title>Rewards</title>
+    <title>Reward</title>
 </svelte:head>
 
+<button class="hero-btn" on:click={generateHero}>Get</button>
 <style></style>
