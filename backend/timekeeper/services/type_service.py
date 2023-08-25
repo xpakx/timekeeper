ee36e5e585113dbf1fe971801b295ed3aa632ac4
xpakx <55586470+xpakx@users.noreply.github.com>
2023-08-25 21:56:42 +0200

    refactor(backend): use dictionary for getting type effectiveness

--- backend/timekeeper/services/type_service.py
+++ backend/timekeeper/services/type_service.py
@@ -1,16 +1,15 @@
 from ..db.models import HeroType
 
+type_table = {
+        HeroType.air: {
+            HeroType.air: 1.0,
+            HeroType.fire: 2.0,
+            HeroType.steam: 0.5,
+            HeroType.water: 1.0,
+            HeroType.earth: 0.5
+            }
+        }
+
 
 def get_effectiveness(attacker: HeroType, attacked: HeroType) -> float:
-    if attacker == HeroType.air:
-        if attacked == HeroType.air:
-            return 1.0
-        elif attacked == HeroType.fire:
-            return 2.0
-        elif attacked == HeroType.steam:
-            return 0.5
-        elif attacked == HeroType.water:
-            return 1.0
-        elif attacked == HeroType.earth:
-            return 0.5
-    return 1.0
+    return type_table[attacker][attacked]
