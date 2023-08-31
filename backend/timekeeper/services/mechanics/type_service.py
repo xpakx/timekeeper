2e6ce5e4a76512f733d4b2fd9e044f2aef8dc0f4
xpakx <55586470+xpakx@users.noreply.github.com>
2023-08-31 21:46:24 +0200

    chore(backend): add type effectiveness data

--- backend/timekeeper/services/mechanics/type_service.py
+++ backend/timekeeper/services/mechanics/type_service.py
@@ -43,6 +43,107 @@ type_table = {
             HeroType.fire: 2.0,
             HeroType.grass: 0.5,
             HeroType.electric: 1
+            },
+        HeroType.rock: {
+            HeroType.fighting: 0.5,
+            HeroType.flying: 2.0,
+            HeroType.ground: 0.5,
+            HeroType.bug: 2.0,
+            HeroType.steel: 0.5,
+            HeroType.fire: 2.0,
+            HeroType.ice: 2.0
+            },
+        HeroType.bug: {
+            HeroType.fighting: 0.5,
+            HeroType.flying: 0.5,
+            HeroType.poison: 0.5,
+            HeroType.ghost: 0.5,
+            HeroType.steel: 0.5,
+            HeroType.fire: 0.5,
+            HeroType.grass: 2.0,
+            HeroType.psychic: 2.0,
+            HeroType.dark: 2.0,
+            },
+        HeroType.ghost: {
+            HeroType.normal: 0,
+            HeroType.ghost: 2.0,
+            HeroType.steel: 0.5,
+            HeroType.psychic: 2.0,
+            HeroType.dark: 0.5
+            },
+        HeroType.steel: {
+            HeroType.rock: 2.0,
+            HeroType.steel: 0.5,
+            HeroType.fire: 0.5,
+            HeroType.water: 0.5,
+            HeroType.electric: 0.5,
+            HeroType.ice: 2.0
+            },
+        HeroType.fire: {
+            HeroType.rock: 0.5,
+            HeroType.bug: 2.0,
+            HeroType.steel: 2.0,
+            HeroType.fire: 0.5,
+            HeroType.water: 0.5,
+            HeroType.grass: 2.0,
+            HeroType.ice: 2.0,
+            HeroType.dragon: 0.5
+            },
+        HeroType.water: {
+            HeroType.ground: 2.0,
+            HeroType.rock: 2.0,
+            HeroType.fire: 2.0,
+            HeroType.water: 0.5,
+            HeroType.grass: 0.5,
+            HeroType.dragon: 0.5
+            },
+        HeroType.grass: {
+            HeroType.flying: 0.5,
+            HeroType.poison: 0.5,
+            HeroType.ground: 2.0,
+            HeroType.rock: 2.0,
+            HeroType.bug: 0.5,
+            HeroType.steel: 0.5,
+            HeroType.fire: 0.5,
+            HeroType.water: 2.0,
+            HeroType.ice: 0.5,
+            HeroType.dragon: 0.5
+            },
+        HeroType.electric: {
+            HeroType.flying: 2.0,
+            HeroType.ground: 0,
+            HeroType.water: 2.0,
+            HeroType.grass: 0.5,
+            HeroType.electric: 0.5,
+            HeroType.dragon: 0.5
+            },
+        HeroType.psychic: {
+            HeroType.fighting: 2.0,
+            HeroType.poison: 2.0,
+            HeroType.steel: 0.5,
+            HeroType.psychic: 0.5,
+            HeroType.dark: 0
+            },
+        HeroType.ice: {
+            HeroType.flying: 2.0,
+            HeroType.ground: 2.0,
+            HeroType.steel: 0.5,
+            HeroType.fire: 0.5,
+            HeroType.water: 0.5,
+            HeroType.grass: 2.0,
+            HeroType.ice: 0.5,
+            HeroType.dragon: 2.0
+            },
+        HeroType.dragon: {
+            HeroType.steel: 0.5,
+            HeroType.dragon: 2.0
+            },
+        HeroType.dark: {
+            HeroType.fighting: 0.5,
+            HeroType.ghost: 2.0,
+            HeroType.steel: 0.5,
+            HeroType.psychic: 2.0,
+            HeroType.dark: 0.5
             }
         }
 
