5f6cfbaa37cfdb228c2b34bc2b3edc71dbbc0ba0
xpakx <55586470+xpakx@users.noreply.github.com>
2023-09-05 16:06:27 +0200

    test(backend): test teaching hero skills

--- backend/tests/test_skill.py
+++ backend/tests/test_skill.py
@@ -6,8 +6,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
 from timekeeper.db.base import Base
 from timekeeper.db.manager import get_db
-from timekeeper.db.models import User, Item, ItemRarity, EquipmentEntry, ItemType
-from timekeeper.db.models import Hero, UserHero
+from timekeeper.db.models import (
+        User,
+        Item,
+        Hero,
+        UserHero,
+        ItemRarity,
+        EquipmentEntry,
+        ItemType,
+        SkillHero,
+        Skill,
+        SkillSet)
 from bcrypt import hashpw, gensalt
 from timekeeper.services.user_service import create_token
 
@@ -68,6 +77,20 @@ def create_user_with_username(username: str) -> int:
     return new_user.id
 
 
+def create_skillset(hero_id: int):
+    db = TestingSessionLocal()
+    entry = SkillSet(
+            hero_id=hero_id,
+            usages_1=0,
+            usages_2=0,
+            usages_3=0,
+            usages_4=0
+            )
+    db.add(entry)
+    db.commit()
+    db.close()
+
+
 def create_user_and_return_id() -> int:
     return create_user_with_username("User")
 
@@ -123,6 +146,31 @@ def create_user_hero(hero_id: int, user_id: int):
             incubated=False
             )
     db.add(item)
+    db.commit()
+    db.refresh(item)
+    db.close()
+    return item.id
+
+
+def create_skill(item_id: int) -> int:
+    db = TestingSessionLocal()
+    skill = Skill(
+            item_id=item_id,
+            )
+    db.add(skill)
+    db.commit()
+    db.refresh(skill)
+    db.close()
+    return skill.id
+
+
+def make_skill_teachable_for(skill_id: int, hero_id: int):
+    db = TestingSessionLocal()
+    hero_skill = SkillHero(
+            skill_id=skill_id,
+            hero_id=hero_id
+            )
+    db.add(hero_skill)
     db.commit()
     db.close()
 
@@ -190,7 +238,8 @@ def test_teaching_skill_with_non_skill_item(test_db):
     user_id = create_user_and_return_id()
     headers = {"Authorization": f"Bearer {get_token_for(user_id)}"}
     hero_id = create_user_hero(create_hero(1, 'Hero'), user_id)
-    create_equipment_item(create_item(10, skill=True), user_id, 1)
+    item_id = create_item(10, skill=False)
+    create_equipment_item(item_id, user_id, 1)
     response = client.post(f"/heroes/{hero_id}/skills",
                            headers=headers,
                            json={
@@ -204,7 +253,8 @@ def test_teaching_skill_with_non_skill_item(test_db):
 def test_teaching_skill_with_no_hero(test_db):
     user_id = create_user_and_return_id()
     headers = {"Authorization": f"Bearer {get_token_for(user_id)}"}
-    create_equipment_item(create_item(10, skill=True), user_id, 1)
+    item_id = create_item(10, skill=True)
+    create_equipment_item(item_id, user_id, 1)
     response = client.post("/heroes/1/skills",
                            headers=headers,
                            json={
@@ -213,3 +263,58 @@ def test_teaching_skill_with_no_hero(test_db):
                                }
                            )
     assert response.status_code == 404
+
+
+def test_teaching_skill_to_hero(test_db):
+    user_id = create_user_and_return_id()
+    headers = {"Authorization": f"Bearer {get_token_for(user_id)}"}
+    hero_id = create_user_hero(create_hero(1, 'Hero'), user_id)
+    create_skillset(hero_id)
+    item_id = create_item(10, skill=True)
+    skill_id = create_skill(item_id)
+    make_skill_teachable_for(skill_id, hero_id)
+    create_equipment_item(item_id, user_id, 1)
+    response = client.post(f"/heroes/{hero_id}/skills",
+                           headers=headers,
+                           json={
+                               "item_id": 10,
+                               "num": 1
+                               }
+                           )
+    assert response.status_code == 200
+
+
+def test_teaching_skill_while_no_skill_found(test_db):
+    user_id = create_user_and_return_id()
+    headers = {"Authorization": f"Bearer {get_token_for(user_id)}"}
+    hero_id = create_user_hero(create_hero(1, 'Hero'), user_id)
+    create_skillset(hero_id)
+    item_id = create_item(10, skill=True)
+    create_equipment_item(item_id, user_id, 1)
+    response = client.post(f"/heroes/{hero_id}/skills",
+                           headers=headers,
+                           json={
+                               "item_id": 10,
+                               "num": 1
+                               }
+                           )
+    print(response.text)
+    assert response.status_code == 500
+
+
+def test_teaching_non_teachable_skill(test_db):
+    user_id = create_user_and_return_id()
+    headers = {"Authorization": f"Bearer {get_token_for(user_id)}"}
+    hero_id = create_user_hero(create_hero(1, 'Hero'), user_id)
+    create_skillset(hero_id)
+    item_id = create_item(10, skill=True)
+    create_skill(item_id)
+    create_equipment_item(item_id, user_id, 1)
+    response = client.post(f"/heroes/{hero_id}/skills",
+                           headers=headers,
+                           json={
+                               "item_id": 10,
+                               "num": 1
+                               }
+                           )
+    assert response.status_code == 400
