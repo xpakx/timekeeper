1ff2a40150a87a6710ffcacdc05c37cf03a22b4b
xpakx <55586470+xpakx@users.noreply.github.com>
2023-09-05 12:23:39 +0200

    fix(backend): fix order of arguments in method call

--- backend/timekeeper/services/skill_service.py
+++ backend/timekeeper/services/skill_service.py
@@ -9,18 +9,23 @@ def teach_hero(
         item_id: int,
         num: int,
         db: Session):
-    item = equipment_repo.get_item_entry(user_id, item_id, db)
+    item = equipment_repo.get_item_entry(item_id, user_id, db)
     if not item or item.amount < 1:
+        print("test")
         raise no_item_exception()
     if not item.item.skill:
+        print("2test")
         raise no_skill_item_exception()
     hero = user_hero_repo.get_hero(user_id, hero_id, db)
     if not hero:
+        print("no hero")
         raise no_such_hero_exception()
     skill = skillset_repo.get_skill(item.item.id, db)
     if not skill:
+        print("no skill")
         raise not_initialized_exception()
     if not skillset_repo.test_skill(hero.id, skill.id, db):
+        print("not teachable")
         raise not_teachable_exception()
     item.amount = item.amount - 1
     skillset_repo.teach_skill(hero.id, skill.id, num, db)
