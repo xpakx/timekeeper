545d1c53d39f6bf0c6ae5a3ab417ce4a74a9ba66
xpakx <55586470+xpakx@users.noreply.github.com>
2023-09-08 22:10:54 +0200

    test(backend): test endpoint for getting teams

---
+++ backend/tests/test_team.py
@@ -0,0 +1,129 @@
+from fastapi.testclient import TestClient
+import pytest
+from timekeeper.main import app
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from timekeeper.db.base import Base
+from timekeeper.db.manager import get_db
+from timekeeper.db.models import (
+        User,
+        Hero,
+        UserHero,
+        ItemRarity,
+        Team)
+from bcrypt import hashpw, gensalt
+from timekeeper.services.user_service import create_token
+
+url = URL.create(
+        "postgresql",
+        "root",
+        "password",
+        "localhost",
+        5432,
+        "time_db_test")
+
+engine = create_engine(
+    url
+)
+TestingSessionLocal = sessionmaker(autocommit=False,
+                                   autoflush=False,
+                                   bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def test_db():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
+
+
+def create_user():
+    db = TestingSessionLocal()
+    password = hashpw("password".encode('utf-8'), gensalt()).decode()
+    new_user = User(
+            username="User",
+            password=password
+            )
+    db.add(new_user)
+    db.commit()
+    db.close()
+
+
+def create_user_with_username(username: str) -> int:
+    db = TestingSessionLocal()
+    password = hashpw("password".encode('utf-8'), gensalt()).decode()
+    new_user = User(
+            username=username,
+            password=password
+            )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    db.close()
+    return new_user.id
+
+
+def create_user_and_return_id() -> int:
+    return create_user_with_username("User")
+
+
+def create_hero(id: int, name: str) -> int:
+    db = TestingSessionLocal()
+    item = Hero(
+            num=id,
+            name=name,
+            description="",
+            rarity=ItemRarity.uncommon
+            )
+    db.add(item)
+    db.commit()
+    db.refresh(item)
+    db.close()
+    return item.id
+
+
+def create_user_hero(hero_id: int, user_id: int):
+    db = TestingSessionLocal()
+    item = UserHero(
+            hero_id=hero_id,
+            owner_id=user_id,
+            incubated=False
+            )
+    db.add(item)
+    db.commit()
+    db.refresh(item)
+    db.close()
+    return item.id
+
+
+def get_token() -> str:
+    return get_token_for(1)
+
+
+def get_token_for(user_id: int) -> str:
+    return create_token({"id": f"{user_id}", "sub": "User"})
+
+
+app.dependency_overrides[get_db] = override_get_db
+client = TestClient(app)
+
+
+# getting team
+def test_getting_team_without_authentication(test_db):
+    response = client.get("/teams")
+    assert response.status_code == 401
+
+
+def test_getting_team_with_wrong_token(test_db):
+    headers = {"Authorization": "Bearer wrong_token"}
+    response = client.get("/teams", headers=headers,)
+    assert response.status_code == 401
