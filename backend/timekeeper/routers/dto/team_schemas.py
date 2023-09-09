a3bca56cacd38d4a89416b9bafc6c3dc2a764632
xpakx <55586470+xpakx@users.noreply.github.com>
2023-09-09 16:38:57 +0200

    fix(backend): fix transforming heroes to list for response

--- backend/timekeeper/routers/dto/team_schemas.py
+++ backend/timekeeper/routers/dto/team_schemas.py
@@ -22,15 +22,20 @@ class TeamResponse(BaseModel):
 
     def transform_data(team: Team):
         transformed_data = {
-                'heroes': [
-                    team.hero_1,
-                    team.hero_2,
-                    team.hero_3,
-                    team.hero_4,
-                    team.hero_5,
-                    team.hero_6
-                    ]
+                'heroes': []
                 }
+        if team.hero_1:
+            transformed_data['heroes'].push(team.hero1)
+        if team.hero_2:
+            transformed_data['heroes'].push(team.hero2)
+        if team.hero_3:
+            transformed_data['heroes'].push(team.hero3)
+        if team.hero_4:
+            transformed_data['heroes'].push(team.hero4)
+        if team.hero_5:
+            transformed_data['heroes'].push(team.hero5)
+        if team.hero_6:
+            transformed_data['heroes'].push(team.hero6)
         return TeamResponse(**transformed_data)
 
     class Config:
