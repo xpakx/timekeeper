44d459018c2b7b7ebc1538d9d9a60a816c063ec1
xpakx <55586470+xpakx@users.noreply.github.com>
2023-09-24 16:03:43 +0200

    feat(frontend): add page for hero view

---
+++ frontend/src/routes/heroes/[id]/+page.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { getToken } from "../../../token-manager";
+    import { page } from "$app/stores";
+    import type { Hero } from "../../../types/Hero";
+    let apiUri = "http://localhost:8000";
+    let message: String;
+    let id = Number($page.params.id);
+    getHero(id);
+    let hero: Hero;
+
+    async function getHero(heroId: number) {
+        let token: String = await getToken();
+        if (!token || token == "") {
+            return;
+        }
+
+        try {
+            let response = await fetch(
+                `${apiUri}/timers/${heroId}/history`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+
+            if (response.ok) {
+                let fromEndpoint = await response.json();
+                hero = fromEndpoint;
+            } else {
+                if (response.status == 401) {
+                    goto("/logout");
+                }
+                const errorBody = await response.json();
+                message = errorBody.detail;
+            }
+        } catch (err) {
+            if (err instanceof Error) {
+                message = err.message;
+            }
+        }
+    }
+</script>
+
+<svelte:head>
+    <title>Hero</title>
+</svelte:head>
+
+{#if hero}
+    <div class="hero-container">
+        {hero.name}
+    </div>
+{/if}
+
+<style>
+    .hero-container {
+        margin-bottom: 10px;
+    }
+</style>
