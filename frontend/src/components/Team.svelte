24a7fc54ab5017b4d0d908f25edacb385b08c89a
xpakx <55586470+xpakx@users.noreply.github.com>
2023-10-02 16:51:49 +0200

    feat(frontend): add buttons for team manipulation

--- frontend/src/components/Team.svelte
+++ frontend/src/components/Team.svelte
@@ -7,26 +7,43 @@
     let message: String;
 
     let active: UserHero | undefined = undefined;
+    let active_num: number | undefined = undefined;
     export let team: UserHero[] = [];
 
     async function addToTeam(hero_id: number, num: number) {
-        changeTeam('add', num, undefined, hero_id);
+        changeTeam("add", num, undefined, hero_id);
+    }
+
+    function switchTo(num: number) {
+        if (!active_num) {
+            return;
+        }
+        switchHeroes(num, active_num);
+        active_num = undefined;
+    }
+
+    function insertAt(num: number) {
+        if (!active) {
+            return;
+        }
+        addToTeam(active.id, num);
+        active = undefined;
     }
 
     async function switchHeroes(num: number, switch_num: number) {
-        changeTeam('switch', num, switch_num, undefined);
+        changeTeam("switch", num, switch_num, undefined);
     }
 
     async function deleteFromTeam(num: number) {
-        changeTeam('delete', num, undefined, undefined);
+        changeTeam("delete", num, undefined, undefined);
     }
-    
+
     async function changeTeam(
         action: String,
         num: number,
         switch_num: number | undefined = undefined,
-        hero_id: number | undefined = undefined,
-        ) {
+        hero_id: number | undefined = undefined
+    ) {
         let token: String = await getToken();
         if (!token || token == "") {
             return;
@@ -35,21 +52,18 @@
             action: action,
             num: num,
             switch_num: switch_num,
-            hero_id: hero_id
-        }
+            hero_id: hero_id,
+        };
 
         try {
-            let response = await fetch(
-                `${apiUri}/teams`,
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${token}`,
-                    },
-                    body: JSON.stringify(body)
-                }
-            );
+            let response = await fetch(`${apiUri}/teams`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify(body),
+            });
 
             if (response.ok) {
                 let fromEndpoint = await response.json();
@@ -70,20 +84,22 @@
 
 {#if team}
     <div class="team-elem">
-        {#each team as hero}
+        {#each team as hero, index}
             <CompactHeroCard {hero} />
+            <div class="buttons-container">
+                <button on:click={() => insertAt(index + 1)}>Add</button>
+                <button on:click={() => switchTo(index + 1)}>Switch</button>
+                <button on:click={() => deleteFromTeam(index + 1)}
+                    >Delete</button
+                >
+            </div>
         {/each}
-        <div class="buttons-container">
-            <button>Add</button>
-            <button>Switch</button>
-            <button>Delete</button>
-        </div>
     </div>
     {#each [1, 2, 3, 4, 5, 6] as containers}
         {#if team.length < containers}
             <div class="team-elem">
                 <div class="buttons-container">
-                    <button>Add</button>
+                    <button on:click={() => insertAt(containers)}>Add</button>
                 </div>
             </div>
         {/if}
