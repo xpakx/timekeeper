3e62c279a477542d6639abb14b1134ef7020afe0
xpakx <55586470+xpakx@users.noreply.github.com>
2023-10-06 10:17:34 +0200

    feat(frontend): let user get battles from backend

---
+++ frontend/src/routes/battle/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { getToken } from "../../token-manager";
+    import { page } from "$app/stores";
+    import type { Battle } from "../../types/Battle";
+    let apiUri = "http://localhost:8000";
+    let message: String;
+    let id = Number($page.params.id);
+    getCurrentBattle();
+    let battle: Battle;
+
+    async function getCurrentBattle() {
+        let token: String = await getToken();
+        if (!token || token == "") {
+            return;
+        }
+
+        try {
+            let response = await fetch(`${apiUri}/battles`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (response.ok) {
+                let fromEndpoint = await response.json();
+                battle = fromEndpoint;
+            } else {
+                if (response.status == 401) {
+                    goto("/logout");
+                }
+                const errorBody = await response.json();
+                message = errorBody.detail;
+            }
+        } catch (err) {
+            if (err instanceof Error) {
+                message = err.message;
+            }
+        }
+    }
+</script>
+
+<svelte:head>
+    <title>Hero</title>
+</svelte:head>
+
+{#if battle}
+    <div class="battle-container" />
+{:else}
+    <div>No active battle</div>
+{/if}
+
+<style>
+    .battle-container {
+        margin-bottom: 10px;
+    }
+</style>
