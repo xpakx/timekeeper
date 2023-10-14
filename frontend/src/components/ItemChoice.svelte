51ceed86ab6e1fa2456e3f24203c257ae9c0ee97
xpakx <55586470+xpakx@users.noreply.github.com>
2023-10-14 23:19:20 +0200

    feat(frontend/ui): style item choice

--- frontend/src/components/ItemChoice.svelte
+++ frontend/src/components/ItemChoice.svelte
@@ -41,16 +41,16 @@
                     goto("/logout");
                 }
                 const errorBody = await response.json();
-                emitMessage(errorBody.detail)
+                emitMessage(errorBody.detail);
             }
         } catch (err) {
             if (err instanceof Error) {
-                emitMessage(err.message)
+                emitMessage(err.message);
             }
         }
     }
 
-    function chooseSkill(id: number) {
+    function chooseItem(id: number) {
         dispatch("choice", { id: id });
     }
 
@@ -61,9 +61,50 @@
 
 <div class="items">
     {#each items as item}
-         {item.item.name}
+        <div class="item">
+            <div class="item-header">
+                <div class="item-name">
+                    {item.item.name}
+                </div>
+                <div class="item-amount">
+                    {item.amount}
+                </div>
+            </div>
+            <button class="item-btn" on:click={() => chooseItem(item.id)}
+                >Select</button
+            >
+        </div>
     {/each}
 </div>
 
 <style>
+    .items {
+        margin-top: 10px;
+        display: flex;
+        font-size: 15px;
+        gap: 10px;
+    }
+
+    .item-header {
+        display: flex;
+        justify-content: space-between;
+        gap: 5px;
+    }
+
+    .item-btn {
+        margin-top: 5px;
+        background-color: #313244;
+        color: #a6adc8;
+        width: 100%;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        border: none;
+        border-radius: 0 0 10px 10px;
+        cursor: pointer;
+    }
+
+    .item-btn:hover {
+        background-color: #6c7086;
+        color: #f2cdcd;
+    }
 </style>
